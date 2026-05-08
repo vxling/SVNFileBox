91dbbef753ddf9ad27d9651dbf260e5ff88b1a8b
Developer <developer@local>
2026-05-08 11:21:57 +0800

    revert: restore all UI to pre-WPF-UI design

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -5,7 +5,6 @@ using System.Windows.Threading;
 using Serilog;
 using SVNFileBox.Services;
 using SVNFileBox.Windows;
-using Wpf.Ui.Appearance;
 
 namespace SVNFileBox;
 
@@ -16,10 +15,6 @@ public partial class App : Application
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
-
-        // Apply WPF-UI theme
-        ApplicationThemeManager.Apply(ApplicationTheme.Light);
-
         var logDir = Path.Combine(
             Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
             "SVNFileBox", "logs");
